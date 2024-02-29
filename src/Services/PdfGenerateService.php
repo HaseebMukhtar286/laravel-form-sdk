b5d4a65647056f0e73e2a3a0e0a7156d9632c52e
Muhammad Haseeb <haseeb.mukhtar@ascend.com.sa>
2024-02-29 11:56:49 +0500

    .

--- src/Services/PdfGenerateService.php
+++ src/Services/PdfGenerateService.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace haseebmukhtar286\LaravelFormSdk\Services;
+
+use haseebmukhtar286\LaravelFormSdk\Models\FormSubmission;
 use haseebmukhtar286\LaravelFormSdk\Services\ApiService;
+
 // use App\Models\Setting;
 
 class PdfGenerateService
@@ -10,12 +13,19 @@ class PdfGenerateService
     public static function pdfGenerate($id)
     {
         if ($id) {
-            $uri = "/form/" . $id;
-            [$result] = ApiService::makeRequest('GET', $uri);
-            $data['schema'] = $result;
-            // $data['settings'] = Setting::where('key', 'TRANSLATION_ARABIC')->first();
-            return response()->json(['data' => $data], 200);
-        } 
-    }
+            $formSubmission = FormSubmission::find($id);
+            if ($formSubmission) {
 
+                $data['formSubmission'] = $formSubmission;
+                $uri = "/form/" . $formSubmission['form_id'];
+
+                [$result] = ApiService::makeRequest('GET', $uri);
+                $data['schema'] = $result;
+                // $data['settings'] = Setting::where('key', 'TRANSLATION_ARABIC')->first();
+                return response()->json(['data' => $data], 200);
+            } else {
+                return response()->json(['data' => 'No Record Found'], 400);
+            }
+        }
+    }
 }
