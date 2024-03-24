0f3f325d576b089f586387decade270d0d84c077
Muhammad Haseeb <haseeb.mukhtar@ascend.com.sa>
2024-03-24 12:57:42 +0500

    .

--- src/Services/FormSchemaService.php
+++ src/Services/FormSchemaService.php
@@ -9,7 +9,7 @@ class FormSchemaService
     public static function create($request)
     {
         $data  = [
-            "form_id" => $request->data->_id,
+            "form_id" => $request->data['_id'],
             "data" => $request->data,
             "schema_version" => $request->schema_version,
         ];
