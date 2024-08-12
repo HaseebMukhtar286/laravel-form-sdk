86bbc00e6f3f4ad9505df8151f23c085e0679cbc
Your Name <you@example.com>
2024-08-12 11:57:10 +0500

    fix

--- src/Services/SchemaService.php
+++ src/Services/SchemaService.php
@@ -10,8 +10,15 @@ class SchemaService
 {
     public static function listingBySecretKey($req)
     {
-        // Build the query string from the $req array
-        $queryString = http_build_query($req);
+        // Manually set the query parameters
+        $queryString = http_build_query([
+            'page' => isset($req['page']) ? $req['page'] : 1,
+            'per_page' => isset($req['per_page']) ? $req['per_page'] : 10,
+            'status' => isset($req['status']) ? $req['status'] : true,
+            'search' => isset($req['search']) ? $req['search'] : '',
+            'fromDate' => isset($req['fromDate']) ? $req['fromDate'] : '',
+            'toDate' => isset($req['toDate']) ? $req['toDate'] : '',
+        ]);
 
         // Append the query string to the URI
         $uri = '/formListingData?' . $queryString;
