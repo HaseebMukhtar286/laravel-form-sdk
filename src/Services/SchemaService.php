7a6fbc9800ef70293d5d939cca71a9796e3fbe74
Your Name <you@example.com>
2024-08-21 16:35:32 +0500

    fix

--- src/Services/SchemaService.php
+++ src/Services/SchemaService.php
@@ -40,7 +40,7 @@ class SchemaService
             "name" => $request->name,
             "icon" => $request->icon,
             "is_template" => $request["is_template"] ?? false,
-            "is_campaign" => $request["is_campaign"] ?? false,
+            "is_campaign" => $request->is_campaign ?? false,
             "start_date" => $request["start_date"] ?? null,
             "end_date" => $request["end_date"] ?? null,
             "meta_data" => $request['meta_data'] ?? null,
