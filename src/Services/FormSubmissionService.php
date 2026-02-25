8cf33734a1bb68bbed198c50f99f82287b24a881
ilyas <muhammad.ilyas>
2026-02-25 15:51:51 +0500

    update

--- src/Services/FormSubmissionService.php
+++ src/Services/FormSubmissionService.php
@@ -118,6 +118,7 @@ class FormSubmissionService
             } elseif (auth()->user()->cluster_ids && (auth()->user()->isClusterManager() || auth()->user()->isHoldCo() || auth()->user()->isSofCluster() || auth()->user()->isSofHoldCo())) {
                 $collection = $collection->whereIn('data.cluster.value', auth()->user()->cluster_ids);
             } elseif (auth()->user()->obligation_sites_ids && auth()->user()->isFacilityManager()) {
+                $collection = $collection->whereRelation('user', 'type', "=", 'facility');
                 $collection = $collection->whereIn('data.site.value', auth()->user()->obligation_sites_ids);
             } else {
                 $collection = $collection->where("user_id", auth()->user()->_id);
